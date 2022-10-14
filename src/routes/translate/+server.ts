eec53f4fac8220d7fc5b4dcd51135db1cb0b8bee
Richard Ivánek <richard.ivanek@gmail.com>
2022-10-14 23:18:46 +0200

    Znaky okolo slov

--- src/routes/translate/+server.ts
+++ src/routes/translate/+server.ts
@@ -29,15 +29,42 @@ readFileSync('cmudict.txt')
 		syllables.forEach((s) => allSyllables.add(s));
 	});
 
-console.log([...allSyllables]);
+const legal = ['a', 'z', '-', "'"].map((c) => c.charCodeAt(0));
+const isLegal = (c: string | number) => {
+	if (typeof c === 'string') c = c.charCodeAt(0);
+	return (c >= legal[0] && c <= legal[1]) || legal.indexOf(c, 2) >= 0;
+};
+const splitMarks = (word: string): [string, string, string] => {
+	let idx = -1;
+
+	for (let i = 0; i < word.length; ++i) {
+		let c = word.charCodeAt(i);
+
+		if (!isLegal(c)) continue;
+
+		let j = i + 1;
+		for (; j < word.length + 1; ++j) {
+			c = word.charCodeAt(j);
+
+			if (!isLegal(c)) break;
+		}
+
+		return [word.slice(0, i), word.slice(i, j), word.slice(j)];
+	}
+
+	return [word, '', ''];
+};
 
 export const POST: RequestHandler = async ({ request }) => {
-	const data: string = await request.text();
+	const data: string = (await request.text()).trim();
 
 	// Prelozeni
 	let text = data
 		.split(' ')
-		.map((word) => dictionary[word.toLowerCase()] ?? word)
+		.map((w) => {
+			const [pre, word, suf] = splitMarks(w.toLowerCase());
+			return pre + (dictionary[word] ?? word) + suf;
+		})
 		.join(' ');
 
 	return json({ text });
